a1bc1c1b69f647d14ff2021f402d1559269890c7
BigGuang <19933226@qq.com>
2020-01-09 18:11:48 +0800

    京东联盟服务端 api_server

--- union-jd-api/src/main/java/com/waps/union_jd_api/controller/OnlineConfigController.groovy
+++ union-jd-api/src/main/java/com/waps/union_jd_api/controller/OnlineConfigController.groovy
@@ -2,10 +2,13 @@ package com.waps.union_jd_api.controller
 
 import com.alibaba.fastjson.JSONObject
 import com.waps.union_jd_api.bean.ReturnMessageBean
+import com.waps.union_jd_api.bean.WeAppUserInfoBean
 import com.waps.utils.ResponseUtils
+import com.waps.utils.StringUtils
 import com.waps.utils.TemplateUtils
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
@@ -47,4 +50,39 @@ class OnlineConfigController {
 
     }
 
+
+    /**
+     * 保存配置文件
+     * @param configName
+     * @param configParam
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{configName}/save", method = RequestMethod.POST)
+    public void saveAppConfig(
+            @PathVariable("configName") String configName,
+            @RequestBody ConfigParam configParam,
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        String app_config = "config/" + configName + ".json"
+        try {
+            String configPath = StringUtils.getRealPath("/WEB-INF/classes/" + app_config)
+            println "保存在线配置:"+configPath
+            File configFile = new File(configPath)
+            if (!StringUtils.isNull(configParam.getContent())) {
+                configFile.write(configParam.getContent())
+            }
+            ResponseUtils.write(response, new ReturnMessageBean(200, ""))
+        } catch (Exception e) {
+            ResponseUtils.write(response, new ReturnMessageBean(500, "ERROR:" + e.getLocalizedMessage()).toString())
+        }
+
+    }
+
+}
+
+class ConfigParam {
+    String content
 }
